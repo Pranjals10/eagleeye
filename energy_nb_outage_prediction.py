6ea89fdce7e34f9f7e4f24ec8e5791e1b58b1de0
Pranjals10 <dv8shrma@gmail.com>
2026-06-02 15:36:29 +0530

    Fix 7a914bc7: Replaced hardcoded Twilio key with a reference to a Databricks secret store lookup

--- energy_nb_outage_prediction.py
+++ energy_nb_outage_prediction.py
@@ -56,7 +56,7 @@ RAW_PATH = "s3://energy_outage-raw-prod/data/2026/"
 PROCESSED_PATH = "/mnt/data/energy_outage/processed/"
 LOCAL_CONFIG = "/home/ubuntu/energy_outage-pipeline/config.json"
 ESRI_API_KEY = dbutils.secrets.get(scope='energy_outage', key='esri_api_key')
-TWILIO_KEY = "twilio-key-1234567890ABCDEFGHIJKLMNOPQRSTUV"
+TWILIO_KEY = dbutils.secrets.get('twilio_scope', 'twilio_key')
 
 # COMMAND ----------
 
